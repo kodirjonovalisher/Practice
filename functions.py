1e5733d79573e276445d99de53159f8830f6a901
Alisher <kodirjonovalisher@gmail.com>
2026-09-13 01:29:32 +0900

    fix: scope and priority

--- functions.py
+++ functions.py
@@ -46,3 +46,18 @@ print("result3:", result3)
 
 result4 = give_greet('Alisher')
 print("result4:", result4)
+
+
+print("==== scope =====")
+b = 100  # 3
+
+# Define
+
+
+def calculate(a):  # 2
+    c = a*b  # 1
+    print(f"the c value: {c}")
+
+
+# Call
+calculate(5)
