09219475d780788f9e3759f45d7e3eeed2f23832
Alisher <kodirjonovalisher@gmail.com>
2026-09-13 01:22:55 +0900

    fix: learn keyword and default arguments in python

--- functions.py
+++ functions.py
@@ -28,3 +28,21 @@ print("result1:", result1)
 
 result2 = greeting("Alex")
 print("result2:", result2)
+
+
+print("====Keyword and default arguments")
+
+# Define
+
+
+def give_greet(name, age=22):
+    print("give_greet is executed")
+    return f"Hi {name}, you are {age} years old"
+
+
+# Call
+result3 = give_greet(name="Alisher", age=28)
+print("result3:", result3)
+
+result4 = give_greet('Alisher')
+print("result4:", result4)
